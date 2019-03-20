a9138fbcb35fa4a5436f2c2296a910f8b762f0e6
Pav0l <pavol.karas@gmail.com>
2019-03-20 10:14:16 +0100

    Commercial middleware

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require('helmet');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
 server.use(express.json());
+// helmet is a commercial middleware that allows you to customize your response header
+// helmet is a function that you need to invoke to return a legit middleware
+// it creates a bunch of headers for our response
+server.use(helmet());
 
 server.use('/api/hubs', hubsRouter);
 
